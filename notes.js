0e011edbfb85d606aaf7a9ffbee571a48d1985c9
Joe Szafarowicz <joeszaf+github@gmail.com>
2019-08-02 20:46:17 -0400

    add chalk to add and remove methods

--- notes.js
+++ notes.js
@@ -1,4 +1,5 @@
 const fs = require('fs')
+const chalk = require('chalk')
 
 const getNotes = () => {
   return "Your notes..."
@@ -16,9 +17,9 @@ const addNote = function(title, body) {
       title: title,
       body: body
     })
-    console.log("New note added!")
+    console.log(chalk.green.inverse("New note added!"))
   } else {
-    console.log("Note title taken!")
+    console.log(chalk.red.inverse("Note title taken!"))
   }
 
   notes.push({
@@ -35,6 +36,13 @@ const removeNote = (title) => {
   const filteredNotes = notes.filter((note) => {
     return note.title !== title
   })
+
+  if (notes.length > filteredNotes.length) {
+    console.log(chalk.green.inverse("Note removed!"))
+    saveNotes(filteredNotes)
+  } else {
+    console.log(chalk.red.inverse("No note found."))
+  }
 
   saveNotes(filteredNotes)
 }
